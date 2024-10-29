a340ddca4bd5f11e9414bb5d2bf4b32676d6c79c
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2024-10-29 02:14:09 +0100

    feat[InspectorWindow][WIP]: display info and components

--- Source/ChironEngine/Source/DataModels/Window/EditorWindow/InspectorWindow.cpp
+++ Source/ChironEngine/Source/DataModels/Window/EditorWindow/InspectorWindow.cpp
@@ -1,7 +1,19 @@
 #include "Pch.h"
 #include "InspectorWindow.h"
 
-InspectorWindow::InspectorWindow() : EditorWindow("Inspector", ImGuiWindowFlags_AlwaysAutoResize)
+#include "Application.h"
+
+#include "Modules/ModuleScene.h"
+
+#include "Factories/ComponentWindowFactory.h"
+
+#include "DataModels/Components/TransformComponent.h"
+
+#include "DataModels/Window/EditorWindow/SubWindows/Inspector/TransformComponentWindow.h"
+
+#include "DataModels/GameObject/GameObject.h"
+
+InspectorWindow::InspectorWindow() : EditorWindow(ICON_FA_CIRCLE_INFO " Inspector", ImGuiWindowFlags_AlwaysAutoResize), _lastSelected(nullptr)
 {
 }
 
@@ -11,4 +23,87 @@ InspectorWindow::~InspectorWindow()
 
 void InspectorWindow::DrawWindowContent(const std::shared_ptr<CommandList>& commandList)
 {
+    auto selected = App->GetModule<ModuleScene>()->GetSelectedGameObject();
+    if (selected)
+    {
+        if (selected != _lastSelected)
+        {
+            _lastSelected = selected;
+            _componentsWindows.clear();
+            FillComponentsWindows();
+
+        }
+        else if (_lastSelected->HowManyComponentsHas() != _componentsWindows.size())
+        {
+            _componentsWindows.clear();
+            FillComponentsWindows();
+        }
+        DrawGameObjectInfo();
+        DrawComponentsWindows(commandList);
+        DrawAddComponent();
+    }
+}
+
+void InspectorWindow::DrawGameObjectInfo()
+{
+    bool isRoot = _lastSelected->GetParent() == nullptr;
+    if (!isRoot)
+    {
+        bool& enabled = _lastSelected->IsEnabled();
+        ImGui::Checkbox("###enableGameObject", &enabled);
+    
+        ImGui::SameLine();
+    }
+
+    float inputTextWidth = ImGui::GetContentRegionAvail().x;
+    if (!isRoot)
+    {
+        inputTextWidth -= ImGui::CalcTextSize("Static").x + 35.0f;
+    }
+    ImGui::PushItemWidth(inputTextWidth > 50.0f ? inputTextWidth : 50.0f);
+    std::string name = _lastSelected->GetName();
+    if (ImGui::InputText("##GameObject", name.data(), 32, ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_AutoSelectAll))
+    {
+        _lastSelected->SetName(name);
+    }
+    ImGui::PopItemWidth();
+
+    if (!isRoot)
+    {
+        ImGui::SameLine();
+        bool isStatic = _lastSelected->IsStatic();
+        if (ImGui::Checkbox("###staticGameObject", &isStatic))
+        {
+            _lastSelected->SetStatic(isStatic);
+        }
+        ImGui::SameLine();
+        ImGui::Text("Static");
+    }
+}
+
+void InspectorWindow::DrawComponentsWindows(const std::shared_ptr<CommandList>& commandList)
+{
+    if (ImGui::BeginChild("Child", ImVec2(0, 0), ImGuiChildFlags_AutoResizeY))
+    {
+        ImGui::SeparatorText("Components");
+        for (auto& componentWindow : _componentsWindows)
+        {
+            componentWindow->Draw(commandList);
+        }
+        ImGui::EndChild();
+    }
+}
+
+void InspectorWindow::DrawAddComponent()
+{
+    ImGui::Separator();
+    ImGui::Text("Add Compoent implementarlo");
+}
+
+void InspectorWindow::FillComponentsWindows()
+{
+    for (auto component : _lastSelected->GetComponents())
+    {
+        _componentsWindows.push_back(ComponentWindowFactory::CreateComponentWindow(component));
+    }
 }
--- Source/ChironEngine/Source/DataModels/Window/EditorWindow/InspectorWindow.h
+++ Source/ChironEngine/Source/DataModels/Window/EditorWindow/InspectorWindow.h
@@ -1,6 +1,9 @@
 #pragma once
 #include "EditorWindow.h"
 
+class GameObject;
+class ComponentWindow;
+
 class InspectorWindow : public EditorWindow
 {
 public:
@@ -9,7 +12,15 @@ public:
 
 private:
     void DrawWindowContent(const std::shared_ptr<CommandList>& commandList) override;
+    void DrawGameObjectInfo();
+    void DrawComponentsWindows(const std::shared_ptr<CommandList>& commandList);
+    void DrawAddComponent();
+
+    void FillComponentsWindows();
 
 private:
+    std::vector<std::unique_ptr<ComponentWindow>> _componentsWindows;
+
+    GameObject* _lastSelected;
 };
 
