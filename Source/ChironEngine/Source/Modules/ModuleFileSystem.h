710fae1c22a8a0d4baa1f2aa5daeab0f04be3a48
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2024-11-25 18:02:54 +0100

    feat: integrate physfs into the engine

--- Source/ChironEngine/Source/Modules/ModuleFileSystem.h
+++ Source/ChironEngine/Source/Modules/ModuleFileSystem.h
@@ -5,6 +5,8 @@ class TextureImporter;
 class ModelImporter;
 class Asset;
 
+struct PHYSFS_File;
+
 class ModuleFileSystem : public Module
 {
 public:
@@ -18,6 +20,25 @@ public:
 
     static const std::string GetFileExtension(const char* path);
     static const std::string GetFileName(const std::string& path);
+    static const std::string GetPathWithoutFile(const std::string& path);
+
+    // ------------- PHYSFS METHODS ----------------------
+
+    static bool SaveFile(const void* buffer, const char* filePath, size_t size);
+    static bool LoadFile(const char* filePath, char*& buffer);
+    static bool DeleteFileC(const char* path);
+    static bool ExistsFile(const char* path);
+    static bool CreateDirectoryC(const char* directoryName);
+    static bool IsDirectory(const char* path);
+
+private:
+    enum class OpenFileMethod
+    {
+        APPEND,
+        READ,
+        WRITE
+    };
+    static bool OpenFile(const char* filePath, OpenFileMethod method, PHYSFS_File*& result);
 
 private:
     std::unique_ptr<TextureImporter> _textureImporter;
