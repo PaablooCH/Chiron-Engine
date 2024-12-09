dc1c223cdea5bc7ba6934400fefcd048288b7d9b
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2024-12-09 20:47:21 +0100

    feat: MeshRendererComponent & Renderable interface

--- Source/ChironEngine/Source/Enums/ComponentType.h
+++ Source/ChironEngine/Source/Enums/ComponentType.h
@@ -3,6 +3,7 @@
 #include "EnumNames.h"
 
 class TransformComponent;
+class MeshRendererComponent;
 
 DECLARE_ENUM_NAMES(ComponentType, TRANSFORM,MESH_RENDERER)
 
@@ -15,4 +16,10 @@ template<>
 struct ComponentToEnum<TransformComponent>
 {
     const static ComponentType value = ComponentType::TRANSFORM;
+};
+
+template<>
+struct ComponentToEnum<MeshRendererComponent>
+{
+    const static ComponentType value = ComponentType::MESH_RENDERER;
 };
