2df6368c4ad8d416d791afed16823cf6c1d16160
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2024-12-18 20:43:44 +0100

    feat: mesh method updated
    import, load and save fixed. The binaries are read correctly and the methods are updated with the assets/resources changes

--- Source/ChironEngine/Source/DataModels/FileSystem/Importers/MeshImporter.cpp
+++ Source/ChironEngine/Source/DataModels/FileSystem/Importers/MeshImporter.cpp
@@ -24,18 +24,12 @@ MeshImporter::~MeshImporter()
 
 void MeshImporter::Import(const char* filePath, const std::shared_ptr<MeshAsset>& mesh)
 {
-    char* filebuffer;
-    ModuleFileSystem::LoadFile(filePath, filebuffer);
-    Load(filebuffer, mesh);
-    Save(mesh);
-    delete filebuffer;
-}
+    char* fileBuffer;
+    ModuleFileSystem::LoadFile(filePath, fileBuffer);
+    
+    char* fileBufferOriginal = fileBuffer;
 
-void MeshImporter::Load(const char* fileBuffer, const std::shared_ptr<MeshAsset>& mesh)
-{
-    auto d3d12 = App->GetModule<ModuleID3D12>();
-    auto copyCommandList = d3d12->GetCommandList(D3D12_COMMAND_LIST_TYPE_COPY);
-    auto directCommandList = d3d12->GetCommandList(D3D12_COMMAND_LIST_TYPE_DIRECT);
+    mesh->SetName(ModuleFileSystem::GetFile(filePath));
 
     unsigned int header[2];
     memcpy(header, fileBuffer, sizeof(header));
@@ -47,122 +41,134 @@ void MeshImporter::Load(const char* fileBuffer, const std::shared_ptr<MeshAsset>
 
     // -------------- VERTEX ---------------------
 
-    Vertex* vertexPointer = new Vertex[numVertices];
-    unsigned int bytes = sizeof(Vertex) * numVertices;
-    memcpy(vertexPointer, fileBuffer, bytes);
-    std::vector<Vertex> triangleVertices(vertexPointer, vertexPointer + numVertices);
+    std::vector<Vertex> triangleVertices(reinterpret_cast<Vertex*>(fileBuffer),
+        reinterpret_cast<Vertex*>(fileBuffer) + numVertices);
+    fileBuffer += sizeof(Vertex) * numVertices;
 
     const UINT vertexBufferSize = static_cast<UINT>(triangleVertices.size() * sizeof(Vertex));
 
     std::string newFileName = "Vertex " + mesh->GetName();
     mesh->SetVertexBuffer(CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize),
-        triangleVertices.size(), newFileName);
-
-    D3D12_SUBRESOURCE_DATA subresourceData = {};
-    subresourceData.pData = triangleVertices.data();
-    subresourceData.RowPitch = vertexBufferSize;
-    subresourceData.SlicePitch = vertexBufferSize;
-    copyCommandList->UpdateBufferResource(mesh->GetVertexBuffer(), 0, 1, &subresourceData);
-
-    fileBuffer += bytes;  
+        triangleVertices, newFileName);
 
     // -------------- INDEX ---------------------
 
-    UINT* indexPointer = new UINT[numIndices];
-    bytes = sizeof(UINT) * numIndices;
-    memcpy(indexPointer, fileBuffer, bytes);
-    std::vector<UINT> indexBufferData(indexPointer, indexPointer + numIndices);
+    std::vector<UINT> indexBufferData(reinterpret_cast<UINT*>(fileBuffer),
+        reinterpret_cast<UINT*>(fileBuffer) + numIndices);
+    fileBuffer += sizeof(UINT) * numIndices;
+
     const UINT indexBufferSize = static_cast<UINT>(indexBufferData.size() * sizeof(UINT));
 
     newFileName = "Index " + mesh->GetName();
-    mesh->SetIndexBuffer(CD3DX12_RESOURCE_DESC::Buffer(indexBufferSize), indexBufferData.size(),
+    mesh->SetIndexBuffer(CD3DX12_RESOURCE_DESC::Buffer(indexBufferSize), indexBufferData,
         DXGI_FORMAT_R32_UINT, newFileName);
 
-    D3D12_SUBRESOURCE_DATA subresourceData2 = {};
-    subresourceData2.pData = indexBufferData.data();
-    subresourceData2.RowPitch = indexBufferSize;
-    subresourceData2.SlicePitch = indexBufferSize;
-    copyCommandList->UpdateBufferResource(mesh->GetIndexBuffer(), 0, 1, &subresourceData2);
+    Save(mesh);
 
-    // Change states
-    directCommandList->TransitionBarrier(mesh->GetIndexBuffer(), D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
-    directCommandList->TransitionBarrier(mesh->GetVertexBuffer(), D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
+    delete[] fileBufferOriginal;
+}
 
-    auto queueType = copyCommandList->GetType();
-    uint64_t initFenceValue = d3d12->ExecuteCommandList(copyCommandList);
-    //d3d12->WaitForFenceValue(queueType, initFenceValue);
-    CHIRON_TODO("Watch out this");
+void MeshImporter::Load(const char* libraryPath, const std::shared_ptr<MeshAsset>& mesh)
+{
+    if (!ModuleFileSystem::ExistsFile(libraryPath))
+    {
+        // ------------- META ----------------------
 
-    queueType = directCommandList->GetType();
-    initFenceValue = d3d12->ExecuteCommandList(directCommandList);
-    //d3d12->WaitForFenceValue(queueType, initFenceValue);
-    CHIRON_TODO("Watch out this");
+        std::string metaPath = mesh->GetAssetPath() + META_EXT;
+        rapidjson::Document doc;
+        Json meta = Json(doc);
+        ModuleFileSystem::LoadJson(metaPath.c_str(), meta);
 
-    delete[] vertexPointer;
-    delete[] indexPointer;
+        // ------------- REIMPORT FILE ----------------------
+
+        std::string assetPath = meta["assetPath"];
+        Import(assetPath.c_str(), mesh);
+
+        return;
+    }
+
+    char* fileBuffer;
+    ModuleFileSystem::LoadFile(libraryPath, fileBuffer);
+
+    char* fileBufferOriginal = fileBuffer;
+
+    unsigned int header[3];
+    memcpy(header, fileBuffer, sizeof(header));
+    fileBuffer += sizeof(header);
+
+    mesh->SetName(std::string(fileBuffer, header[0]));
+    fileBuffer += header[0];
+
+    UINT numVertices = header[0];
+    UINT numIndices = header[1];
+
+    fileBuffer += sizeof(header);
+
+    // -------------- VERTEX ---------------------
+
+    std::vector<Vertex> triangleVertices(reinterpret_cast<Vertex*>(fileBuffer),
+        reinterpret_cast<Vertex*>(fileBuffer) + numVertices);
+    fileBuffer += sizeof(Vertex) * numVertices;
+
+    const UINT vertexBufferSize = static_cast<UINT>(triangleVertices.size() * sizeof(Vertex));
+
+    std::string newFileName = "Vertex " + mesh->GetName();
+    mesh->SetVertexBuffer(CD3DX12_RESOURCE_DESC::Buffer(vertexBufferSize),
+        triangleVertices, newFileName);
+
+    // -------------- INDEX ---------------------
+
+    std::vector<UINT> indexBufferData(reinterpret_cast<UINT*>(fileBuffer),
+        reinterpret_cast<UINT*>(fileBuffer) + numIndices);
+    fileBuffer += sizeof(UINT) * numIndices;
+
+    const UINT indexBufferSize = static_cast<UINT>(indexBufferData.size() * sizeof(UINT));
+
+    newFileName = "Index " + mesh->GetName();
+    mesh->SetIndexBuffer(CD3DX12_RESOURCE_DESC::Buffer(indexBufferSize), indexBufferData,
+        DXGI_FORMAT_R32_UINT, newFileName);
+
+    delete[] fileBufferOriginal;
 }
 
 void MeshImporter::Save(const std::shared_ptr<MeshAsset>& mesh)
 {
-    auto vertexBuffer = mesh->GetVertexBuffer();
-    auto indexBuffer = mesh->GetIndexBuffer();
-                        //Vertex
-    unsigned int size = static_cast<UINT>(vertexBuffer->GetVertexStride() * vertexBuffer->GetNumVertex()) + 
-        //Indices
-        (sizeof(UINT) * indexBuffer->GetNumIndices());
+    auto& triangle = mesh->GetTriangleVertices();
+    auto& index = mesh->GetIndexData();
 
-    unsigned int header[2] = { static_cast<unsigned int>(vertexBuffer->GetNumVertex()), 
-        static_cast<unsigned int>(indexBuffer->GetNumIndices()) };
+                        //Vertex                                      //Indices
+    size_t size = static_cast<UINT>(sizeof(Vertex) * triangle.size()) + (sizeof(UINT) * index.size());
+
+    unsigned int header[3] = { static_cast<unsigned int>(mesh->GetName().size()), 
+        static_cast<unsigned int>(triangle.size()), static_cast<unsigned int>(index.size()) };
     size += sizeof(header);
 
-    char* filebuffer = new char[size] {};
-    char* cursor = filebuffer;
+    size += sizeof(char) * static_cast<unsigned int>(mesh->GetName().size());
+
+    char* fileBuffer = new char[size] {};
+    char* cursor = fileBuffer;
 
     unsigned int bytes = sizeof(header);
     memcpy(cursor, header, bytes);
     cursor += bytes;
 
+    bytes = sizeof(char) * static_cast<unsigned int>(mesh->GetName().size());
+    memcpy(cursor, &mesh->GetName()[0], bytes);
+    cursor += bytes;
+
     // -------------- VERTEX ---------------------
 
-    bytes = static_cast<UINT>(vertexBuffer->GetVertexStride() * vertexBuffer->GetNumVertex());
-
-    void* pData = nullptr;
-    HRESULT hr = vertexBuffer->GetResource()->Map(0, nullptr, &pData);
-    if (SUCCEEDED(hr))
-    {
-        const void* vertexData = static_cast<const void*>(pData);
-
-        memcpy(cursor, vertexData, bytes);
-
-        vertexBuffer->GetResource()->Unmap(0, nullptr);
-    }
-    else
-    {
-        LOG_ERROR("Couldn't map the mesh resource to save all the vertexs.");
-    }
+    bytes = static_cast<UINT>(sizeof(Vertex) * triangle.size());
+    memcpy(cursor, triangle.data(), bytes);
     cursor += bytes;
 
     // -------------- INDEX ---------------------
 
-    bytes = sizeof(UINT) * static_cast<UINT>(indexBuffer->GetNumIndices());
+    bytes = sizeof(UINT) * static_cast<UINT>(index.size());
+    memcpy(cursor, index.data(), bytes);
+    cursor += bytes;
 
-    pData = nullptr;
-    hr = indexBuffer->GetResource()->Map(0, nullptr, &pData);
-    if (SUCCEEDED(hr))
-    {
-        const void* indexData = static_cast<const void*>(pData);
-        //size_t dataSize = indexBuffer->GetIndexBufferView().SizeInBytes;
+    ModuleFileSystem::SaveFile(mesh->GetLibraryPath().c_str(), fileBuffer, size);
 
-        memcpy(cursor, indexData, bytes);
-
-        indexBuffer->GetResource()->Unmap(0, nullptr);
-    }
-    else
-    {
-        LOG_ERROR("Couldn't map the mesh resource to save all the indices.");
-    }
-
-    ModuleFileSystem::SaveFile(mesh->GetLibraryPath().c_str(), filebuffer, size);
-
-    delete[] filebuffer;
+    delete[] fileBuffer;
 }
