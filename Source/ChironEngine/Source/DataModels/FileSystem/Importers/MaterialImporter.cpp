c34911820707e85fca56b6e13fd27446cb539345
Pablo Cebollada Hernández <paabloo.ch@gmail.com>
2024-12-18 20:44:16 +0100

    feat[MaterialImporter]: methods updated
    import, load and save fixed. The binaries are read correctly and the methods are updated with the assets/resources changes

--- Source/ChironEngine/Source/DataModels/FileSystem/Importers/MaterialImporter.cpp
+++ Source/ChironEngine/Source/DataModels/FileSystem/Importers/MaterialImporter.cpp
@@ -3,13 +3,10 @@
 
 #include "Application.h"
 
-#include "Modules/ModuleFileSystem.h"
 #include "Modules/ModuleResources.h"
 
 #include "DataModels/Assets/MaterialAsset.h"
 #include "DataModels/Assets/TextureAsset.h"
-
-#include "Defines/FileSystemDefine.h"
 
 MaterialImporter::MaterialImporter()
 {
@@ -21,87 +18,128 @@ MaterialImporter::~MaterialImporter()
 
 void MaterialImporter::Import(const char* filePath, const std::shared_ptr<MaterialAsset>& material)
 {
+    material->SetName(ModuleFileSystem::GetFile(filePath));
+
     rapidjson::Document doc;
     Json json = Json(doc);
     ModuleFileSystem::LoadJson(filePath, json);
+
+    auto resources = App->GetModule<ModuleResources>();
     
     std::string path = json["BaseTexturePath"];
-    if (path == "")
+    if (path != "")
     {
-
+        material->SetBaseTexture(resources->RequestAsset<TextureAsset>(path));
     }
 
     path = json["NormalMapPath"];
-    if (path == "")
+    if (path != "")
     {
-
+        material->SetNormalMap(resources->RequestAsset<TextureAsset>(path));
     }
 
     path = json["AmbientOcclusionPath"];
-    if (path == "")
+    if (path != "")
     {
-
+        material->SetAmbientOcclusion(resources->RequestAsset<TextureAsset>(path));
     }
 
     path = json["PropertyTexturePath"];
-    if (path == "")
+    if (path != "")
     {
-
+        material->SetPropertyTexture(resources->RequestAsset<TextureAsset>(path));
     }
 
     path = json["EmissiveTexturePath"];
-    if (path == "")
+    if (path != "")
     {
-
+        material->SetEmissiveTexture(resources->RequestAsset<TextureAsset>(path));
     }
 
-    Color baseColor = Color(json["BaseColor"]["R"], json["BaseColor"]["G"], json["BaseColor"]["B"], json["BaseColor"]["A"]);
-    material->SetBaseColor(baseColor);
-    Color specularColor = Color(json["SpecularColor"]["R"], json["SpecularColor"]["G"], json["SpecularColor"]["B"], json["SpecularColor"]["A"]);
-    material->SetSpecularColor(specularColor);
-    UINT options = json["Options"];
-    material->SetOptions(options);
     Save(material);
 }
 
-void MaterialImporter::Load(const char* fileBuffer, const std::shared_ptr<MaterialAsset>& material)
+void MaterialImporter::Load(const char* libraryPath, const std::shared_ptr<MaterialAsset>& material)
 {
+    if (!ModuleFileSystem::ExistsFile(libraryPath))
+    {
+        // ------------- META ----------------------
+
+        std::string metaPath = material->GetAssetPath() + META_EXT;
+        rapidjson::Document doc;
+        Json meta = Json(doc);
+        ModuleFileSystem::LoadJson(metaPath.c_str(), meta);
+
+        Color baseColor = Color(meta["BaseColor"]["R"], meta["BaseColor"]["G"], meta["BaseColor"]["B"], meta["BaseColor"]["A"]);
+        material->SetBaseColor(baseColor);
+        Color specularColor = Color(meta["SpecularColor"]["R"], meta["SpecularColor"]["G"], meta["SpecularColor"]["B"], meta["SpecularColor"]["A"]);
+        material->SetSpecularColor(specularColor);
+        UINT options = meta["Options"];
+        material->SetOptions(options);
+
+        // ------------- REIMPORT FILE ----------------------
+
+        std::string assetPath = meta["assetPath"];
+        Import(assetPath.c_str(), material);
+
+        return;
+    }
+
+    char* fileBuffer;
+    ModuleFileSystem::LoadFile(libraryPath, fileBuffer);
+    char* fileBufferOriginal = fileBuffer;
+
     // ------------- BINARY ----------------------
 
-    unsigned int bytes = sizeof(UID);
+    auto resourceModule = App->GetModule<ModuleResources>();
+
+    unsigned int header[1];
+    unsigned int bytes = sizeof(header);
+    memcpy(header, fileBuffer, bytes);
+    fileBuffer += bytes;
+
+    material->SetName(std::string(fileBuffer, header[0]));
+    fileBuffer += header[0];
+
+    bytes = sizeof(UID);
     UID textureUID;
     memcpy(&textureUID, fileBuffer, bytes);
     if (textureUID != 0)
     {
-        material->SetBaseTexture(nullptr);
+        auto texture = resourceModule->SearchAsset<TextureAsset>(textureUID);
+        material->SetBaseTexture(texture);
     }
     fileBuffer += bytes;
 
     memcpy(&textureUID, fileBuffer, bytes);
     if (textureUID != 0)
     {
-        material->SetNormalMap(nullptr);
+        auto texture = resourceModule->SearchAsset<TextureAsset>(textureUID);
+        material->SetNormalMap(texture);
     }
     fileBuffer += bytes;
     
     memcpy(&textureUID, fileBuffer, bytes);
     if (textureUID != 0)
     {
-        material->SetAmbientOcclusion(nullptr);
+        auto texture = resourceModule->SearchAsset<TextureAsset>(textureUID);
+        material->SetAmbientOcclusion(texture);
     }
     fileBuffer += bytes;
     
     memcpy(&textureUID, fileBuffer, bytes);
     if (textureUID != 0)
     {
-        material->SetPropertyTexture(nullptr);
+        auto texture = resourceModule->SearchAsset<TextureAsset>(textureUID);
+        material->SetPropertyTexture(texture);
     }
     fileBuffer += bytes;
     
     memcpy(&textureUID, fileBuffer, bytes);
     if (textureUID != 0)
     {
-        material->SetEmissiveTexture(nullptr);
+        auto texture = resourceModule->SearchAsset<TextureAsset>(textureUID);
+        material->SetEmissiveTexture(texture);
     }
     fileBuffer += bytes;
 
@@ -119,6 +157,8 @@ void MaterialImporter::Load(const char* fileBuffer, const std::shared_ptr<Materi
     UINT options;
     memcpy(&options, fileBuffer, bytes);
     material->SetOptions(options);
+
+    delete[] fileBufferOriginal;
 }
 
 void MaterialImporter::Save(const std::shared_ptr<MaterialAsset>& material)
@@ -127,45 +167,6 @@ void MaterialImporter::Save(const std::shared_ptr<MaterialAsset>& material)
 
     rapidjson::Document doc;
     Json json = Json(doc);
-    std::string path = "";
-    auto texture = material->GetBaseTexture();
-    if (texture)
-    {
-        path = texture->GetAssetPath();
-    }
-    json["BaseTexturePath"] = path;
-    path.clear();
-    
-    auto texture = material->GetNormalMap();
-    if (texture)
-    {
-        path = texture->GetAssetPath();
-    }
-    json["NormalMapPath"] = path;
-    path.clear();
-    
-    auto texture = material->GetAmbientOcclusion();
-    if (texture)
-    {
-        path = texture->GetAssetPath();
-    }
-    json["AmbientOcclusionPath"] = path;
-    path.clear();
-    
-    auto texture = material->GetPropertyTexture();
-    if (texture)
-    {
-        path = texture->GetAssetPath();
-    }
-    json["PropertyTexturePath"] = path;
-    path.clear();
-    
-    auto texture = material->GetEmissiveTexture();
-    if (texture)
-    {
-        path = texture->GetAssetPath();
-    }
-    json["EmissiveTexturePath"] = path;
 
     json["BaseColor"]["R"] = material->GetBaseColor().R();
     json["BaseColor"]["G"] = material->GetBaseColor().G();
@@ -186,10 +187,23 @@ void MaterialImporter::Save(const std::shared_ptr<MaterialAsset>& material)
                 //textures' uid     //base/specular color  //options    
     UINT size = (sizeof(UID) * 5) + sizeof(Color) * 2 + sizeof(UINT);
 
+    unsigned int header[1] = { static_cast<unsigned int>(material->GetName().size()) };
+
+    size += sizeof(header);
+    size += sizeof(char) * static_cast<unsigned int>(material->GetName().size());
+
     char* fileBuffer = new char[size] {};
     char* cursor = fileBuffer;
 
-    unsigned int bytes = sizeof(UID);
+    unsigned int bytes = sizeof(header);
+    memcpy(cursor, header, bytes);
+    cursor += bytes;
+
+    bytes = sizeof(char) * static_cast<unsigned int>(material->GetName().size());
+    memcpy(cursor, &material->GetName()[0], bytes);
+    cursor += bytes;
+
+    bytes = sizeof(UID);
     UID textureUID = material->GetBaseTexture() ? material->GetBaseTexture()->GetUID() : 0;
     memcpy(cursor, &textureUID, bytes);
     cursor += bytes;
